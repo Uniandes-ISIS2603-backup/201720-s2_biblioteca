0caa6eea9c2df3e7ecdfd46f932ada1b10559b30
f.posada <f.posada@ISIS2603S2-0043.sis.virtual.uniandes.edu.co>
2017-09-10 17:47:12 -0500

    Se inicia la creacion de los test de usuariopersistence

---
+++ biblioteca-back/src/test/java/co/edu/uniandes/quantum/biblioteca/persistence/UsuarioPersistenceTest.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.quantum.biblioteca.persistence;
+
+import co.edu.uniandes.quantum.biblioteca.entities.UsuarioEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author f.posada
+ */
+@RunWith(Arquillian.class)
+public class UsuarioPersistenceTest {
+    
+    /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Usuario, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(UsuarioEntity.class.getPackage())
+                .addPackage(UsuarioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Inyección de la dependencia a la clase UsuarioPersistence cuyos métodos
+     * se van a probar.
+     */
+    @Inject
+    private UsuarioPersistence persistence;
+
+    /**
+     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
+     * datos por fuera de los métodos que se están probando.
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+
+     /**
+     * Entities a usar
+     */
+    private List<UsuarioEntity> data = new ArrayList<UsuarioEntity>();
+    
+    private void clearData() {
+        em.createQuery("delete from UsuarioEntity").executeUpdate();
+    }
+
+
+ private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            UsuarioEntity entity = factory.manufacturePojo(UsuarioEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+    
+    public UsuarioPersistenceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+         try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of create method, of class UsuarioPersistence.
+     */
+    @Test
+    public void testCreate() throws Exception {
+        PodamFactory factory = new PodamFactoryImpl();
+    UsuarioEntity newEntity = factory.manufacturePojo(UsuarioEntity.class);
+    UsuarioEntity result = persistence.create(newEntity);
+
+    Assert.assertNotNull(result);
+    UsuarioEntity entity = em.find(UsuarioEntity.class, result.getId());
+    Assert.assertNotNull(entity);
+    Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+
+    /**
+     * Test of update method, of class UsuarioPersistence.
+     */
+    @Test
+    public void testUpdate() throws Exception {
+        fail("");
+    }
+
+    /**
+     * Test of delete method, of class UsuarioPersistence.
+     */
+    @Test
+    public void testDelete() throws Exception {
+        fail("");
+    }
+
+    /**
+     * Test of find method, of class UsuarioPersistence.
+     */
+    @Test
+    public void testFind() throws Exception {
+        fail("");
+    }
+
+    /**
+     * Test of findAll method, of class UsuarioPersistence.
+     */
+    @Test
+    public void testFindAll() throws Exception {
+        fail("");
+    }
+    
+}
