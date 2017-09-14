58266adeccfb363eb986f92dea0c86f509418e6c
davidarturolr <31114498+davidarturolr@users.noreply.github.com>
2017-09-14 17:53:16 -0500

    kjhvgcszdxfgchvbj,

---
+++ biblioteca-back/src/main/java/co/edu/uniandes/quantum/biblioteca/entities/BlogEntity.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.quantum.biblioteca.entities;
+
+import java.io.Serializable;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author da.leon
+ */
+public class BlogEntity extends BaseEntity implements Serializable{
+    
+       @ManyToOne
+    private UsuarioEntity miUsuario;
+       
+    /**
+     * titulo del libro sobre el cual se crea el blog
+     */   
+    private String tituloLibro;
+    
+    /**
+     * cambia el titulo del libro
+     * @param nuevoTitulo nuevo titulo del libro
+     */
+   public void setTituloLibro(String nuevoTitulo)
+   {
+       tituloLibro = nuevoTitulo;
+              
+   }
+   
+   /**
+    * obtiene el titulo del libro
+    * @return devuelve el titulo del libro
+    */
+   public String getTituloLibro()
+  {
+      
+    return tituloLibro;
+               
+  
+  }
+    
+    
+    
+}
