cb0bd49fd05f75bd6c3873ed8f2bdad3c34861e3
Daniel <de.agudelo@uniandes.edu.co>
2017-09-18 09:28:00 -0500

    DTOs DetailDTO y Persistence hechas para SIstemaBiblioteca, falta PersistenceTEst pero es que no me ha servido entonces no lo pusheo.

---
+++ biblioteca-web/src/main/java/co/edu/uniandes/quantum/biblioteca/dtos/SistemaBibliotecaDTO.java
@@ -0,0 +1,42 @@
+package co.edu.uniandes.quantum.biblioteca.dtos;
+
+import co.edu.uniandes.quantum.biblioteca.entities.SistemaBibliotecaEntity;
+
+public class SistemaBibliotecaDTO {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SistemaBibliotecaDTO(){
+
+    }
+    public SistemaBibliotecaDTO(SistemaBibliotecaEntity entity){
+    this.id = entity.getId();
+    this.name = entity.getName();
+
+    }
+
+    public SistemaBibliotecaEntity toEntity(){
+        SistemaBibliotecaEntity entity = new SistemaBibliotecaEntity();
+        entity.setId(this.id);
+        entity.setName(this.name);
+
+        return entity;
+    }
+
+}
