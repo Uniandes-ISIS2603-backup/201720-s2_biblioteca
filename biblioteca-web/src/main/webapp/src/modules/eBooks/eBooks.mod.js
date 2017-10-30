27f6d2b8ad3bcb0ce144dede6c7da7118072aefb
Daniel Agudelo Cruz <de.agudelo@uniandes.edu.co>
2017-10-30 12:25:28 -0500

    eBook y eVideo .json creados, todos los archivos de ambos módulos están hechos, pero vacíos. Están llenos eBooks.list y eBooks.mod.

---
+++ biblioteca-web/src/main/webapp/src/modules/eBooks/eBooks.mod.js
@@ -0,0 +1,25 @@
+function (ng) {
+    // Definición del módulo
+    var mod = ng.module("eBookModule", ['ui.router']);
+
+    // Configuración de los estados del módulo
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        // En basePath se encuentran los templates y controladores de módulo
+        var basePath = 'src/modules/eBooks/';
+        // Mostrar la lista de autores será el estado por defecto del módulo
+        //$urlRouterProvider.otherwise("/eBooksList");
+        // Definición del estado 'eBooksList' donde se listan los autores
+        $stateProvider.state('eBooksList', {
+            // Url que aparecerá en el browser
+            url: '/eBooks/list',
+            views: {
+                'mainView': {
+                    templateUrl: basePath + 'eBooks.list.html',
+                    controller: 'eBookCtrl',
+                    controllerAs: 'ctrl'
+                }
+            }
+        });
+    }
+    ]);
+})(window.angular);
