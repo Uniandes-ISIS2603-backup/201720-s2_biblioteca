91d825f62da99e858ec76ca3e2970427d375cf27
jf.garcia <jf.garcia@ISIS2603S2-0029.sis.virtual.uniandes.edu.co>
2017-10-31 11:42:04 -0500

    Correcciones en video, no obstante el hipervínculo al correrlo sigue sin funcionar.

--- biblioteca-web/src/main/webapp/src/modules/video/videos.ctrl.js
+++ biblioteca-web/src/main/webapp/src/modules/video/videos.ctrl.js
@@ -1,11 +1,18 @@
 (function (ng) {
     var mod = ng.module("videosModule");
-    mod.constant("videosContext", "api/videos");
-    mod.controller('videosCtrl', ['$scope', '$http', 'videosContext',
-        function ($scope, $http, videosContext) {
-            $http.get('data/video.json').then(function (response) {
+    mod.constant("videosContext", "api/999/videos");
+    mod.controller('videosCtrl', ['$scope', '$http', 'videosContext', '$state',
+        function ($scope, $http, videosContext, $state) {
+            $http.get(videosContext).then(function (response) {
                 $scope.videoRecords = response.data;
             });
+            
+            if ($state.params.videoId !== undefined && ($state.params.videoId !== null)) {
+                $http.get(videosContext + '/' + $state.params.videoId).then(function (response) {
+                      $scope.videoRecords = response.data.videos;
+                  $scope.currentVideo = response.data;
+                });
+            }
         }
     ]);
 }
--- biblioteca-web/src/main/webapp/src/modules/video/videos.mod.js
+++ biblioteca-web/src/main/webapp/src/modules/video/videos.mod.js
@@ -1,15 +1,16 @@
 (function (ng) {
     // Definición del módulo
     var mod = ng.module("videosModule", ['ui.router']);
-
+ mod.constant("videosContext", "api/999/videos");
     // Configuración de los estados del módulo
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             // En basePath se encuentran los templates y controladores de módulo
             var basePath = 'src/modules/video/';
-            // Mostrar la lista de videos será el estado por defecto del módulo
+            // Mostrar la lista de autores será el estado por defecto del módulo
             $urlRouterProvider.otherwise("/videosList");
             // Definición del estado 'videosList' donde se listan los videos
             $stateProvider.state('videos', {
+                // Url que aparecerá en el browser
                 url: '/videos',
                 abstract: true,
                 views: {
@@ -19,7 +20,7 @@
                         controllerAs: 'ctrl'
                     }
                 }
-            }).state('videosList', {
+             }).state('videosList', {
                 url: '/list',
                 parent: 'videos',
                 views: {
@@ -27,16 +28,14 @@
                         templateUrl: basePath + 'videos.list.html'
                     }
                 }
-            }).state('videosDetail', {
-                url: '/{videosId:int}/detail',
+            }).state('videoDetail', {
+                url: '/{videoId:int}/detail',
                 parent: 'videos',
                 param: {
-                    videosId: null
+                    videoId: null
                 },
                 views: {
-                    'listView': {
-                        templateUrl: basePath + 'videos.list.html'
-                    },
+                    
                     'detailView': {
                         templateUrl: basePath + 'videos.detail.html',
                         controller: 'videosCtrl',
@@ -46,6 +45,5 @@
                 }
 
             });
-        }
-    ]);
+        }]);
 })(window.angular);
