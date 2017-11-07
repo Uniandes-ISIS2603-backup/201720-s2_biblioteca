9e8e9349e309544a863f93b714e5630d481460b3
Daniel <de.agudelo@uniandes.edu.co>
2017-11-07 11:07:51 -0500

    Poblado de la tabla de los eResources y arreglito pequeño en los list y detail.

--- biblioteca-web/sql/biblioteca.sql
+++ biblioteca-web/sql/biblioteca.sql
@@ -7,12 +7,13 @@ delete from comentarioEntity;
 delete from bibliotecaEntity;
 delete from blogEntity;
 delete from ebookEntity;
+delete from ecomentarioEntity;
 delete from evideoEntity;
 delete from recursoEntity;
 delete from reservaEntity_recursoEntity;
 delete from salaEntity;
 delete from videoEntity;
-delete  from usuarioEntity;
+delete from usuarioEntity;
 
 
 
@@ -23,6 +24,13 @@ insert into usuarioEntity (id,direccion,name,telefono) values (1,'Direccion1','N
 insert into usuarioEntity (id,direccion,name,telefono) values (2,'Direccion2','Nombre2','Telefono2');
 
 insert into mediopagoEntity (id,name,descripcion,tipo,miusuario_id) values (1,NULL,'0000 1111 2222',1,1);
+
+insert into ebookEntity (id, name, autor, numeroPaginas) values (1,'El tio petros y la conjetur de goldbach' , 'Aposotolos Dioxiadis', 172);
+insert into ebookEntity (id, name, autor, numeroPaginas) values (2,'Calculo Vectorial' , 'Jerrold Marsden, Antony Tromba ', 666);
+
+
+insert into evideoEntity (id, name, autor, duracion) values (1,'Curso AngularJS - Introduccion' , 'codigoFacilito', 10);
+insert into evideoEntity (id, name, autor, duracion) values (2,'Curso AngularJS - Hola Mundo' , 'codigoFacilito', 5);
 
 
 insert into libroEntity(id,aniopublicacion,autor,name,numpaginas) values (1,1996,'James','Moby Dick',123);
