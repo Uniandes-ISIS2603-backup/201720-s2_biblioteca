4616eafaa8b60cec8cf614a5f7ce2fb23be4bf30
Federico Posada <f.posada@ISIS2603S2-0043.sis.virtual.uniandes.edu.co>
2017-11-08 17:47:45 -0500

    Un prestamo en el sql

--- biblioteca-web/sql/biblioteca.sql
+++ biblioteca-web/sql/biblioteca.sql
@@ -20,7 +20,7 @@ delete from usuarioEntity;
 insert into bibliotecaEntity (id,name,ubicacion) values (1,'VirgilioBarco','Calle63');
 
 
-insert into usuarioEntity (id,direccion,name,telefono) values (1,'Direccion1','Nombre1','Telefono1');
+insert into usuarioEntity (id,direccion,name,telefono) values (1,'Cra 75','Juan Perez','11112222');
 insert into usuarioEntity (id,direccion,name,telefono) values (2,'Direccion2','Nombre2','Telefono2');
 
 insert into mediopagoEntity (id,name,descripcion,tipo,miusuario_id) values (1,NULL,'0000 1111 2222',1,1);
@@ -28,12 +28,13 @@ insert into mediopagoEntity (id,name,descripcion,tipo,miusuario_id) values (1,NU
 insert into ebookEntity (id, name, autor, numeroPaginas) values (1,'El tio petros y la conjetur de goldbach' , 'Aposotolos Dioxiadis', 172);
 insert into ebookEntity (id, name, autor, numeroPaginas) values (2,'Calculo Vectorial' , 'Jerrold Marsden, Antony Tromba ', 666);
 
+insert into prestamoentity (id,fechainicio,fechafinal,generomulta,externo,retornado,miusuario_id) values (1,'2017-11-09','2017-11-23',0,1,0,1);
 
 insert into evideoEntity (id, name, autor, duracion) values (1,'Curso AngularJS - Introduccion' , 'codigoFacilito', 10);
 insert into evideoEntity (id, name, autor, duracion) values (2,'Curso AngularJS - Hola Mundo' , 'codigoFacilito', 5);
 
 
-insert into libroEntity(id,aniopublicacion,autor,name,numpaginas) values (1,1996,'James','Moby Dick',123);
+insert into libroEntity(id,aniopublicacion,autor,name,numpaginas, miprestamo_id) values (1,1996,'James','Moby Dick',123,1);
 insert into libroEntity(id,aniopublicacion,autor,name,numpaginas) values (2,1997,'Javier','Oliver Twist',123);
 insert into libroEntity(id,aniopublicacion,autor,name,numpaginas,mibiblioteca_id) values (3,1998,'Jax','Twister',123,1);
 
