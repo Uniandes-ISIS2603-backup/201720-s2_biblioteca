436eb2eae304ebb9c2806c9c2da0a6eab67eb4da
Federico Posada <f.posada@ISIS2603S2-0043.sis.virtual.uniandes.edu.co>
2017-11-12 19:56:57 -0500

    update con fecha mejorado y la lista se ve mas bonita

--- biblioteca-web/src/main/webapp/src/modules/prestamo/new/prestamo.new.ctrl.js
+++ biblioteca-web/src/main/webapp/src/modules/prestamo/new/prestamo.new.ctrl.js
@@ -4,9 +4,13 @@
     mod.controller('prestamoNewCtrl', ['$scope', '$http', 'prestamosContext' , '$state', '$rootScope',
         function ($scope, $http, prestamosContext, $state, $rootScope) {
             $scope.createPrestamo = function () {
+                var feIn=$scope.anioFechaInicio+'-'+$scope.mesFechaInicio+'-'+$scope.diaFechaInicio;
+                var feFi=$scope.anioFechaFinal+'-'+$scope.mesFechaFinal+'-'+$scope.diaFechaFinal;
+                console.info(feIn);
+                console.info(feFi);
                 $http.post('api/999/usuarios/' + $state.params.usuarioId+'/prestamos', {
-                    fechaInicio: $scope.prestamoFechaInicio,
-                    fechaFinal: $scope.prestamoFechaFinal,
+                    fechaInicio: feIn,
+                    fechaFinal: feFi,
                     externo: $scope.prestamoExterno
                 }).then(function (response) {
                     //Prestamo created successfully
